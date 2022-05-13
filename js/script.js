ed350c5526e8bb88a2f4e7c7b7fac87731ebca80
Ajith Kumar <ganeshajith980@gmail.com>
2022-05-13 18:18:02 +0530

    Changed

--- js/script.js
+++ js/script.js
@@ -14,31 +14,4 @@ $(function(){
     });
 
 
-    var controller = new ScrollMagic.Controller();
-
-		// define movement of panels
-		var wipeAnimation = new TimelineMax()
-			// animate to second panel
-			.to("#project-container", 0.5, {z: -150})		// move back in 3D space
-			.to("#project-container", 1,   {x: "-25%"})	// move in to first panel
-			.to("#project-container", 0.5, {z: 0})				// move back to origin in 3D space
-			// animate to third panel
-			.to("#project-container", 0.5, {z: -150, delay: 1})
-			.to("#project-container", 1,   {x: "-50%"})
-			.to("#project-container", 0.5, {z: 0})
-			// animate to forth panel
-			.to("#project-container", 0.5, {z: -150, delay: 1})
-			.to("#project-container", 1,   {x: "-75%"})
-			.to("#project-container", 0.5, {z: 0});
-
-		// create scene to pin and link animation
-		new ScrollMagic.Scene({
-				triggerElement: "#project-wrap",
-				triggerHook: "onLeave",
-				duration: "500%"
-			})
-			.setPin("#project-wrap")
-			.setTween(wipeAnimation)
-			.addIndicators() // add indicators (requires plugin)
-			.addTo(controller);
-  })
+  });
--- js/slider.js
+++ js/slider.js
@@ -1,36 +1,36 @@
-<<<<<<< HEAD
 $(window).on("load", function () {
+  var vW = $(window).width();
     if (vW < 992) {
-        $(".portfolios-wrap").addClass("container");
+        $(".ox10-all-projects").addClass("container");
       }
 });
 jQuery(document).ready(function ($) {
     (function ($) {
       "use strict";
-      if ($(".portfolios").length) {
+      if ($(".ox10-projects").length) {
         var vW = $(window).width();
         var vH = $(window).height();
         if (vW > 991) {
-          $(".portfolios").css("paddingLeft", vW + "px");
+          $(".ox10-projects").css("paddingLeft", vW + "px");
           var portfolioController = new ScrollMagic.Controller();
           var portfolioW =
-            $(".portfolios a").outerWidth() * $(".portfolios a").length;
-          var item = $(".portfolios");
+            $(".ox10-projects a").outerWidth() * $(".ox10-projects a").length;
+          var item = $(".ox10-projects");
           var action = gsap
             .timeline()
             .to(item, { x: -(portfolioW + vW) + "px", ease: Linear.easeNone });
           var portfolioScene = new ScrollMagic.Scene({
-            triggerElement: ".portfolio-hook",
+            triggerElement: ".ox10-projects-hook",
             triggerHook: 0.15,
             duration: portfolioW - vW,
             reverse: !0,
           })
   
-            .setPin(".portfolio", { pushFollowers: !0 })
+            .setPin(".ox10-projects-section", { pushFollowers: !0 })
             .addTo(portfolioController);
   
           var portfolioVscroll = new ScrollMagic.Scene({
-            triggerElement: ".portfolio",
+            triggerElement: ".ox10-projects-section",
             triggerHook: 0.8,
             duration: portfolioW - vW / 2,
             reverse: !0,
@@ -41,48 +41,4 @@ jQuery(document).ready(function ($) {
         }
       }
     })(jQuery);
-=======
-$(window).on("load", function () {
-    if (vW < 992) {
-        $(".portfolios-wrap").addClass("container");
-      }
-});
-jQuery(document).ready(function ($) {
-    (function ($) {
-      "use strict";
-      if ($(".portfolios").length) {
-        var vW = $(window).width();
-        var vH = $(window).height();
-        if (vW > 991) {
-          $(".portfolios").css("paddingLeft", vW + "px");
-          var portfolioController = new ScrollMagic.Controller();
-          var portfolioW =
-            $(".portfolios a").outerWidth() * $(".portfolios a").length;
-          var item = $(".portfolios");
-          var action = gsap
-            .timeline()
-            .to(item, { x: -(portfolioW + vW) + "px", ease: Linear.easeNone });
-          var portfolioScene = new ScrollMagic.Scene({
-            triggerElement: ".portfolio-hook",
-            triggerHook: 0.15,
-            duration: portfolioW - vW,
-            reverse: !0,
-          })
-  
-            .setPin(".portfolio", { pushFollowers: !0 })
-            .addTo(portfolioController);
-  
-          var portfolioVscroll = new ScrollMagic.Scene({
-            triggerElement: ".portfolio",
-            triggerHook: 0.8,
-            duration: portfolioW - vW / 2,
-            reverse: !0,
-          })
-          
-            .setTween(action)
-            .addTo(portfolioController);
-        }
-      }
-    })(jQuery);
->>>>>>> 046fa834a37944472739a9541e48aaa13c5df555
   });
