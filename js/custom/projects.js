a9a6277e038714d47c00d361d7f7ec8802701c7e
Ajith Kumar <ganeshajith980@gmail.com>
2022-05-28 20:23:32 +0530

    Updated

--- js/custom/projects.js
+++ js/custom/projects.js
@@ -1,63 +1,18 @@
-$(document).ready(function(){      
-    var url = window.location.href;
-    var index = url.indexOf('#');
-    var value = (index > 0) ? url.substring(index+1) : "";
-    if(value.length > 0){
-        $('.nav-list-link').removeClass("ox-10-active-link");
-        $('.nav-list-link').addClass("ox10-nav-link");
-        if(value === "all-projects"){
-            $('#all').addClass("ox-10-active-link");
-           }
-           else if(value === "events"){
-            $('#project1').addClass("ox-10-active-link");
-           }
-           else if(value === "branding"){
-            $('#project2').addClass("ox-10-active-link");
-           }
-           else if(value === "product-launches"){
-            $('#project3').addClass("ox-10-active-link");
-           }
+$('.filters ul li').click(function(){
+    console.log(this);
+    $('.filters ul li').removeClass('active');
+    $(this).addClass('active');
+    
+    var data = $(this).attr('data-filter');
+    $grid.isotope({
+      filter: data
+    })
+  });
+  
+  var $grid = $(".grid").isotope({
+    itemSelector: ".all",
+    percentPosition: true,
+    masonry: {
+      columnWidth: ".all"
     }
-   
-
- });
-$('.nav-list-link').click(function(){
-    var value = ($(this).attr('value'));
-    if(value === 'all-projects'){
-        $("#all-projects").show();
-        $("#events").hide();
-        $("#branding").hide();        
-        $("#product-launches").hide();
-        
-    }
-    else if(value === 'events'){
-        $("#all-projects").hide();
-        $("#events").show();
-        $("#branding").hide();        
-        $("#product-launches").hide();
-        
-    }
-    else if(value === 'branding'){
-        $("#all-projects").hide();
-        $("#events").hide();
-        $("#branding").show();        
-        $("#product-launches").hide();
-        
-    }
-    else if(value === 'product-launches'){
-        $("#all-projects").hide();
-        $("#events").hide();
-        $("#branding").hide();        
-        $("#product-launches").show();
-        
-    }
-});
-
-$(document).ready(function(){      
-    $('.nav-list-link').click(function() {
-        $('.nav-list-link').removeClass("ox-10-active-link");
-        $('.nav-list-link').addClass("ox10-nav-link");
-        $(this).addClass("ox-10-active-link");
-       });
- });
-
+  })
--- js/custom/script.js
+++ js/custom/script.js
@@ -17,18 +17,57 @@ $('.nav-link').click(function(){
 
 $(function(){
   var totalItems = $('.testimonial-carousel .carousel-item').length;
-  var currentIndex = $('.testimonial-carousel .carousel-item.active').index() + 1;
+  var currentIndex = $('.testimonial-carousel .carousel-item.active').index();
   $('.ox10-carousel-count').html('' + currentIndex + '/' + totalItems + '');
 
   $('#testimonial-carousel').carousel({
     interval: 2000
-  });
+  })
 
-  $('#testimonial-carousel').bind('slid', function() {
+  $('#testimonial-carousel').on('slide.bs.carousel', function() {
     console.log('hello');
-    currentIndex = $('.testimonial-carousel .carousel-item.active').index() + 1;
+
+    currentIndex = $('.testimonial-carousel .carousel-item.active').index();
+    console.log("Total Items:", totalItems);
+    console.log("CurrentIndex:",currentIndex);
     $('.ox10-carousel-count').html('' + currentIndex + '/' + totalItems + '');
   });
 
+
+});
+
+/* About us - scroll count increase */
+var a = 0;
+$(window).scroll(function() {
+  var nav = $('#ox10-about-us-counter');
+  if (nav.length) {
+    var oTop = nav.offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() > oTop) {
+      $('.ox10-count').each(function() {
+        var $this = $(this),
+          countTo = $this.attr('data-count');
+        $({
+          countNum: $this.text()
+        }).animate({
+            countNum: countTo
+          },
+
+          {
+
+            duration: 3000,
+            easing: 'swing',
+            step: function() {
+              $this.text(Math.floor(this.countNum) + "+");
+            },
+            complete: function() {
+              $this.text(this.countNum + "+");
+              //alert('finished');
+            }
+
+          });
+      });
+      a = 1;
+    }
+  }
 
 });
